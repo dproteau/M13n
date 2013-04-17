a4e6804e32378ae88646a5904b89d36e44a0aa58
David Proteau <david.proteau@gmail.com>
2013-04-17 13:29:44 -0400

    Upgraded Glimpse to the newest version

--- src/M13n/GlimpseSecurityPolicy.cs
+++ src/M13n/GlimpseSecurityPolicy.cs
@@ -1,8 +1,7 @@
 /*
 // Uncomment this class to provide custom runtime policy for Glimpse
 
-using System.Web;
-using Glimpse.Core;
+using Glimpse.AspNet.Extensions;
 using Glimpse.Core.Extensibility;
 
 namespace M13n
@@ -12,8 +11,9 @@ namespace M13n
         public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
         {
             // You can perform a check like the one below to control Glimpse's permissions within your application.
-			// var context = policyContext.GetRequestContext<HttpContextBase>();
-            // if (!context.User.IsInRole("Administrator"))
+			// More information about RuntimePolicies can be found at http://getglimpse.com/Help/Custom-Runtime-Policy
+			// var httpContext = policyContext.GetHttpContext();
+            // if (!httpContext.User.IsInRole("Administrator"))
 			// {
             //     return RuntimePolicy.Off;
 			// }
